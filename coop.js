b5df0717180c9e26928f0801fd17a698cb38e564
ReidMatthew <msreid92@gmail.com>
2021-07-09 16:26:28 -0400

    class init

--- coop.js
+++ coop.js
@@ -1,16 +1,10 @@
 module.exports = class Coop {
     constructor(row, col, tStates) {
-        this.observed = false;
+        this.isCollapsed = false;
         this.row = row;
         this.col = col;
         this.tStates = tStates;
     }
 
-    getSide(sideNumber) {
-
-    }
-
-    checkSide(sideNumber, compareSide) {
-
-    }
+    
 }
---
+++ stack.js
@@ -0,0 +1,15 @@
+module.exports = class Stack {
+    constructor() {
+        this.s = [];
+    }
+
+    push(coop) {
+        s.push(coop);
+    }
+
+    pop() {
+        let subject = s.pop();
+
+        //eval the neighbors if changed
+    }
+}
